8004e8f32a3ab4d94d17eab5797625849f97a82f
florian-KM <mtopiflorian@gmail.com>
2023-12-18 15:51:43 +0100

    fixed footer

---
+++ js/internationalization.js
@@ -0,0 +1,262 @@
+
+let langdata = {
+    "En": {
+        "strings": {
+            "navMenu.home": "Home",
+            "navMenu.about": "Aboout",
+            "navMenu.services": "Services",
+            "navMenu.team": "Team",
+            "navMenu.contact": "Contact",
+
+            "header.welcome": "Welcome to <span>Komunityoo</span>",
+            "header.manage_community": "Manage communities with ease!",
+            "header.get_started": "Get Started",
+            "header.watch_video": "Watch Video",
+            "header.komunityoo": "Komunityoo",
+
+            "service.section.highly_configurable": "Highly configurable",
+            "service.section.customize": "Customize the use of the community by member according to your policy",
+            "service.section.report": "Detailed report",
+            "service.section.get_log": "Get a log of your community's activity at any time over a period of time",
+            "service.section.highly_performance": "High performance",
+            "service.section.every_operation": "Every operation is done quickly, accurately for the best user experience",
+            "service.section.connectivity": "Connectivity",
+            "service.section.stay_close": "Stay close to your community, no matter where you are",
+
+            "about.section.about": "About",
+            "about.section.find": "Find Out More",
+            "about.section.about_us": "About Us",
+            "about.section.find_out": "Find out who we are, our goals and our vision for helping to manage communities.",
+            "about.section.secure": "A secure web application that helps you manage your community or group with simple clicks.",
+            "about.section.komunityoo1": "Komunityoo is a secure web application that helps you manage your community or group with ease! It is developed using recent technologies that allows the application to load quickly and perform well on all types of devices (computers, tablets and phones). This application allows you to build and preserve the history of your community or your group. It makes the group dynamic: members get notification, they can connect to their account, read announcements, check their balance, add credit to their account, see transactions of their account, etc.... ",
+            "about.section.komunityoo2": "Moreover, the application helps you manage events occurring in the community and documents, create survey to collect opinions of your members on topics before making decision, manage community insurance, etc…",
+
+            "counts.section.communities": "Communities",
+            "counts.section.happy_members": "Happy Members",
+            "counts.section.transactions": "Transactions Per Day",
+            "counts.section.plans": "Plans",
+
+            "services.section.services": "Services",
+            "services.section.check_our": "Check our <span>services</span>",
+            "services.section.komunityoo": "Komunityoo offers you a wide range of features to allow you to better manage your community",
+            "services.section.financial_report": "Financial reports",
+            "services.section.you_can": "You can view your community's detailed financial reports in real time",
+            "services.section.member_access": "Member access",
+            "services.section.configure": "Configure your members' access: everyone only uses what he is allowed to use",
+            "services.section.family": "Families Management",
+            "services.section.group": "Group your members together as a family to facilitate their management.",
+            "services.section.cash": "Cash Flow",
+            "services.section.manage": "Manage all of your community's accounts and financial transactions",
+            "services.section.executive": "Executive office",
+            "services.section.setup": "Set up an executive office to assist you in managing the community",
+            "services.section.community_events": "Community events",
+            "services.section.easily": "Easily organize events in your community and follow their evolution over time",
+            "services.section.community_survey":"Surveys",
+            "services.section.easily.survey": "Collect and study the different opinions of your members with surveys",
+            "services.section.community_njangui":"Njangui",
+            "services.section.easily_njangui": "Create, configure and implement your various njangui with ease",
+            "services.section.community_insurance": "Community insurance",
+            "services.section.easily.insurance": "Manage insurance for all families in your community",
+
+            "pricing.section.pricing": "Pricing",
+            "pricing.section.check_our": "Check our <span>pricing</span>",
+            "pricing.section.discover": "Discover all our subscription offers to select the perfect plan for you and your community",
+            "pricing.section.free": "Basic",
+            "pricing.section.month": "/ month",
+            "pricing.section.buy_now": "Buy Now",
+            "pricing.section.business": "Standard",
+            "pricing.section.developer": "Developer",
+            "pricing.section.advanced": "Advanced",
+            "pricing.section.ultimate": "Premium",
+
+            "pricing.features.members": "Members Management",
+            "pricing.features.events": "Events Management",
+            "pricing.features.offices": "Executive Office Managament",
+            "pricing.features.documents": "Document And Event's Reports",
+            "pricing.features.insurances": "Community Insurance Managament",
+            "pricing.features.reports": "Financial Reports",
+            "pricing.features.chats": "Private And Group Chat",
+            "pricing.features.loans": "Financial Helps Managament",
+            "pricing.features.njanguis": "Njanguis Managament",
+            "pricing.features.surveys": "Survey Managament",
+            "pricing.features.bstorage": "2 Go of Storage Available",
+            "pricing.features.sstorage": "5 Go of Storage Available",
+            "pricing.features.pstorage": "8 Go of Storage Available",
+            "pricing.features.bmembers": "Up To 50 Members Allowed",
+            "pricing.features.smembers": "Up To 100 Members Allowed",
+            "pricing.features.pmembers": "Up To 300 Members Allowed",
+            "pricing.features.backups": "Daily Backup",
+            "pricing.features.supports": "24/7 Tech Support",
+            "pricing.section.more": "Please contact us if your community has more than 300 members",
+
+            "contact.section.contact": "Contact",
+            "contact.section.contact_us": "Contact Us",
+            "contact.section.message": "A message from you will always please us. Contact us and we will answer you as soon as possible",
+            "contact.section.adress": "Our Address",
+            "contact.section.email_us": "Email Us",
+            "contact.section.call_us": "Call Us",
+            "contact.section.your_message": "Your message has been sent. Thank you!",
+            "contact.section.send_message": "Send Message",
+            "contact.section.loading": "Loading",
+
+            "footer.join": "Join Our Newsletter",
+            "footer.be_always": "Be always informed of our news by subscribing to our newsletter",
+            "footer.phone": "Phone:",
+            "footer.useful": "Useful Links",
+            "footer.email": "Email:",
+            "footer.home": "Home",
+            "footer.services": "Services",
+            "footer.terms": "Terms of service",
+            "footer.privacy": "Privacy policy",
+            "footer.about_us": "About us",
+            "footer.Our_history": "Our history",
+            "footer.ecosystem": "The ecosystem",
+            "footer.parteners": "Partners",
+            "footer.social_network": "Our Social Networks",
+            "footer.stay": "Stay close to us through all our different accounts on social networks so that you don't miss anything in our evolution",
+            "footer.all_right": "All Rights Reserved",
+            "footer.suscribe": "Suscribe"
+        }
+    },
+    
+    "Fr": {
+        "strings": {
+            "navMenu.home": "Accueil",
+            "navMenu.about": "A propos",
+            "navMenu.services": "Services",
+            "navMenu.team": "Equipe",
+            "navMenu.contact": "Contact",
+
+            "header.welcome": "Bienvenue sur <span>Komunityoo</span>",
+            "header.manage_community": "Gérer les communautés en toute simplicité!",
+            "header.get_started": "Démarrer",
+            "header.watch_video": "Regarder la vidéo",
+            "header.komunityoo": "Komunityoo",
+
+            "service.section.highly_configurable": "Hautement configurable",
+            "service.section.customize": "Personnalisez l'utilisation de la communauté par les membres en fonction de votre politique.",
+            "service.section.report": "Rapport détaillé",
+            "service.section.get_log": "Obtenez un journal de l'activité de votre communauté à tout moment sur une période donnée.",
+            "service.section.highly_performance": "Haute performance",
+            "service.section.every_operation": "Chaque opération est effectuée rapidement et avec précision pour une expérience utilisateur optimale.",
+            "service.section.connectivity": "Connectivité",
+            "service.section.stay_close": "Restez proche de votre communauté, où que vous soyez.",
+
+            "about.section.about": "À propos",
+            "about.section.find": "Pour en savoir plus",
+            "about.section.about_us": "A propos de nous",
+            "about.section.find_out": "Découvrez qui nous sommes, nos objectifs et notre vision du rapprochement des peuples.",
+            "about.section.secure": "Une application web sécurisée qui permet de gérer facilement et simplement une communauté ou un groupe.",
+            "about.section.komunityoo1": "Komunityoo est une application web sécurisée qui permet de gérer facilement et simplement une communauté ou un groupe.Elle est développée en utilisant des technologies récentes qui permettent à l'application de se charger rapidement sur tous les types d'appareils (ordinateurs, tablettes et téléphones). Cette application permet de construire et de conserver l'historique des communautés ou des groupes. Elle rend le groupe dynamique : les membres peuvent se connecter à leur compte sur la communauté, lire les annonces, consulter le solde de leur compte, ajouter du crédit, voir les transactions de leur compte, etc....",
+            "about.section.komunityoo2": "De plus, l'application vous aide à gérer les événements survenant dans la communauté et les documents, à créer des enquêtes pour recueillir les opinions de vos membres sur des sujets avant de prendre une décision, à gérer l'assurance de la communauté, etc...",
+
+            "counts.section.communities": "Communautés",
+            "counts.section.happy_members": "Membres heureux",
+            "counts.section.transactions": "Transactions par jour",
+            "counts.section.plans": "Offres",
+
+            "services.section.services": "Services",
+            "services.section.check_our": "Consultez nos <span>services</span>",
+            "services.section.komunityoo": "Komunityoo vous offre un large éventail de fonctionnalités pour vous permettre de mieux gérer votre communauté",
+            "services.section.financial_report": "Rapports financiers",
+            "services.section.you_can": "Vous pouvez consulter les rapports financiers détaillés de votre communauté en temps réel.",
+            "services.section.member_access": "Accès pour les membres",
+            "services.section.configure": "Configurez l'accès de vos membres : chacun n'utilise que ce qu'il est autorisé à utiliser",
+            "services.section.family": "Gestion des familles",
+            "services.section.group": "Regroupez vos membres en tant que famille pour faciliter leur gestion.",
+            "services.section.cash": "Cash Flow",
+            "services.section.manage": "Gérer tous les comptes et toutes les transactions financières de votre communauté",
+            "services.section.executive": "Bureau exécutif",
+            "services.section.setup": "Mettre en place un bureau exécutif pour vous aider à gérer la communauté.",
+            "services.section.community_events": "Événements communautaires",
+            "services.section.easily": "Organisez facilement des événements dans votre communauté et suivez leur évolution dans le temps.",
+            "services.section.community_survey":"Sondages",
+            "services.section.easily.survey": "Collectez et étudiez les differents avis de vos membres grace aux sondages",
+            "services.section.community_njangui":"Tontines",
+            "services.section.easily_njangui": "Créez, configurez et realisez vos diverses tontines en toute simplicté",
+            "services.section.community_insurance": "Assurance communautaire",
+            "services.section.easily.insurance": "Gérez les assurances de toutes les familles de votre communauté",
+
+            "pricing.section.pricing": "Offres",
+            "pricing.section.check_our": "Consultez nos <span>offres</span>",
+            "pricing.section.discover": "Découvrez toutes nos offres d'abonnement pour choisir la formule idéale pour vous et votre communauté.",
+            "pricing.section.free": "Basique",
+            "pricing.section.month": "/ mois",
+            "pricing.section.buy_now": "Acheter maintenant",
+            "pricing.section.business": "Standard",
+            "pricing.section.developer": "Développeur",
+            "pricing.section.advanced": "Avancé",
+            "pricing.section.ultimate": "Premium",
+
+            "pricing.features.members": "Gestion Des Membres",
+            "pricing.features.events": "Gestion Des Evènements",
+            "pricing.features.offices": "Gestion Du Bureau Executif",
+            "pricing.features.documents": "Documents Et Rapports D'évènements",
+            "pricing.features.insurances": "Gestion De l'Assurance Communautaire",
+            "pricing.features.reports": "Rapports Financier",
+            "pricing.features.chats": "Discussion Privée Et De Groupe",
+            "pricing.features.loans": "Gestion Des Entraides Financières",
+            "pricing.features.njanguis": "Gestion Des Tontines",
+            "pricing.features.surveys": "Gestion Des Sondages",
+            "pricing.features.bstorage": "2 Go De Stockage Disponible",
+            "pricing.features.sstorage": "5 Go De Stockage Disponible",
+            "pricing.features.pstorage": "8 Go De Stockage Disponible",
+            "pricing.features.bmembers": "Jusqu'a 50 Membres autorisés",
+            "pricing.features.smembers": "Jusqu'a 100 Membres autorisés",
+            "pricing.features.pmembers": "Jusqu'a 300 Membres autorisés",
+            "pricing.features.backups": "Sauvegarde Des Données Journalière",
+            "pricing.features.supports": "24/7 Support Technique",
+            "pricing.section.more": "Veuillez nous contacter si votre communauté compte plus de 300 membres.",
+
+            "contact.section.contact": "Contact",
+            "contact.section.contact_us": "Contactez-nous",
+            "contact.section.message": "Un message de votre part nous fera toujours plaisir. Contactez-nous et nous vous répondrons dans les plus brefs délais.",
+            "contact.section.adress": "Notre adresse",
+            "contact.section.email_us": "Envoyez-nous un courriel",
+            "contact.section.call_us": "Appelez-nous",
+            "contact.section.your_message": "Votre message a été envoyé. Merci !",
+            "contact.section.send_message": "Envoyer un message",
+            "contact.section.loading": "Chargement",
+
+            "footer.join": "Inscrivez-vous à notre newsletter",
+            "footer.be_always": "Soyez toujours informé de nos actualités en vous inscrivant à notre newsletter",
+            "footer.phone": "Téléphone:",
+            "footer.email": "Courriel :",
+            "footer.useful": "Liens Utiles",
+            "footer.home": "Accueil",
+            "footer.services": "Services",
+            "footer.terms": "Conditions d'utilisation",
+            "footer.privacy": "Politique de confidentialité",
+            "footer.about_us": "A propos de nous",
+            "footer.Our_history": "Notre histoire",
+            "footer.ecosystem": "L'écosystème",
+            "footer.parteners": "Partenaires",
+            "footer.social_network": "Nos réseaux sociaux",
+            "footer.stay": "Restez proche de nous grâce à nos différents comptes sur les réseaux sociaux afin de ne rien manquer",
+            "footer.all_right": "Tous droits réservés",
+            "footer.suscribe": "S'inscrire"
+
+        }
+    }
+
+}
+
+$("select").change(function () {
+    var x = document.getElementsByTagName('select');
+
+    // let currentLangue = $('body').hasClass('mobile-nav-active') ? x[1].value : x[0].value;
+    let currentLangue ="En";
+
+    document.querySelectorAll(`[data-key]`).forEach(element => {
+        let key = element.getAttribute('data-key');
+        if (key) {
+            element.innerHTML = langdata[currentLangue].strings[key];
+        }
+    });
+
+    if ($('body').hasClass('mobile-nav-active')) {
+        $('body').removeClass('mobile-nav-active');
+        $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
+        $('.mobile-nav-overly').fadeOut();
+    }
+});
